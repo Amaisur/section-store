ddeb1a22e3a76b446ffe5c6f136ce541a8a26342
muscled-clients <muscled.clients1@gmail.com>
2025-07-04 00:14:41 +0500

    asda

--- assets/d5-cart.js
+++ assets/d5-cart.js
@@ -2,7 +2,7 @@
 (function () {
   const cart = document.querySelector('.cart-drawer-d5');
   const cartIcon = document.querySelector('#cart-icon-bubble');
-
+  const rewardsBar = cart.querySelector('[shipping-bar-d5]');
   cartIcon.addEventListener('click', (e) => {
     e.preventDefault();
     cart.classList.add('cd-drawer__open');
@@ -36,6 +36,8 @@
       cartInner.innerHTML = newDrawer.innerHTML;
       addEventListenersToCart();
     }
+    const dataW = rewardsBar.getAttribute('data-width');
+    rewardsBar.style.width = dataW;
   }
 
 document.querySelectorAll('form[action="/cart/add"]').forEach(form => {
